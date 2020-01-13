867d0a427a0999d97eca63af1060258c857fdaf8
Mykola Shchipskyi <m.schipskyi@gmail.com>
2020-01-13 20:34:30 +0200

    Add project files.

---
+++ ITEAHub.Hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITEAHub.Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
